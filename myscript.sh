aca37b704f0dcd7458589f0078ce873a2f54c1c1
Alexander <7086600@gmail.com>
2020-06-26 23:26:39 +0300

    All to github

---
+++ myscript.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "There are some conventions to follow to ensure that your computer is able to find and execute your Bash scripts."
+hi="Hello, everyone!"
+echo $hi. Welcome to our World!
